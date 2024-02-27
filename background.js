a5926b3a608266b12777f73b535b363efb265170
aenstad01 <65510470+aenstad01@users.noreply.github.com>
2024-02-27 15:53:37 -0600

    Update background.js

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ chrome.runtime.onStartup.addListener(initializeExtension);
 
 function initializeExtension() {
   chrome.management.getAll(extensions => {
-    const extensionA = extensions.find(extension => extension.name === "Extension A Name" && extension.enabled);
+    const extensionA = extensions.find(extension => extension.name === "GA4 Fixer" && extension.enabled);
     // Set 'ExtensionADisabled' in storage to true if Extension A is found and enabled, false otherwise.
     chrome.storage.sync.set({ 'extensionADisabled': !!extensionA });
   });
